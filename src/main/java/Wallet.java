3ebacbc135023a37ffe7249c36495865d8bcfe45
kenny-antonio <hei.kenny.4@gmail.com>
2025-11-21 16:45:26 +0300

    Initial commit - Wallet project with Checkstyle and CI

---
+++ src/main/java/Wallet.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Wallet {
+    private String color;
+    private String size;
+    private double money;
+    private boolean isOpen;
+    private boolean isLost;
+
+    public Wallet(String color, String size) {
+        this.color = color;
+        this.size = size;
+        this.money = 0;
+        this.isOpen = false;
+        this.isLost = false;
+    }
+
+    public void open() {
+        this.isOpen = true;
+    }
+
+    public void close() {
+        this.isOpen = false;
+    }
+
+    public void addMoney(double amount) {
+        if (!isOpen) {
+            System.out.println("Wallet is closed!");
+            return;
+        }
+        this.money += amount;
+    }
+
+    public double getMoney() {
+        return this.money;
+    }
+
+    public boolean isLost() {
+        return this.isLost;
+    }
+
+    public void setLost(boolean lost) {
+        this.isLost = lost;
+    }
+
+    public void checkMoney() {
+        System.out.println("Money: " + this.money);
+    }
+}
+
